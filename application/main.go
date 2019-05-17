3237ec917cbe999a9d6f920ae7be3995c018c689
Patrick Selge <patrick.selge@daparto.com>
2019-05-17 14:43:26 +0200

    add go app

---
+++ application/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
